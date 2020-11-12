ddd358963a89e849c638ea5ebd049c0c813c9670
MarioP <mario.pecher.mpr@googlemail.com>
2020-11-12 13:29:29 +0100

    Add files via upload

---
+++ mk_dbn921_state.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/ksh
+# determine e1 app server state
+rg_srv="sdacdbn921"
+rg_host=$(ssh root@$rg_srv hostname)
+node=$(hostname)
+if [ $rg_host == $node ];
+then
+
+                STATUS=0
+                echo "$STATUS ${rg_srv}_home HOME=$rg_host OK - Resource Group $rg_srv.rg is running on node $node"
+else
+        STATUS=3
+	echo "$STATUS ${rg_srv}_home HOME=$rg_host OK - Node $node is not Resource Group home" 
+fi;
